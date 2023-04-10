ef58a426faef84d3ed40a29f88f373514138b9ca
Whisky  shusuky <whisky.shusuky@gmail.com>
2023-04-11 00:18:23 +0900

    text boxとsubmitボタンだけ追加

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [prompt, setPrompt] = useState('');
+  const [completion, setCompletion] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('/completions', { prompt });
+      setCompletion(response.data.completion);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Prompt:
+          <input type="text" value={prompt} onChange={(e) => setPrompt(e.target.value)} />
+        </label>
+        <button type="submit">Complete</button>
+      </form>
+      <p>Completion: {completion}</p>
     </div>
   );
 }
