1c7e89067b286de5869720cac6c9d51aff753e6d
Whisky  shusuky <whisky.shusuky@gmail.com>
2023-04-29 12:23:55 +0900

    メールもpostする

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Form, Button } from 'react-bootstrap';
 
 function App() {
-  const [prompt, setPrompt] = useState('');
+  const [data, setData] = useState({
+    prompt: '',
+    email: '',
+  });
   const [completion, setCompletion] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:8080', { prompt }, { headers: { 'Content-Type': 'application/json' } });
+      const response = await axios.post('http://localhost:8080', data, { headers: { 'Content-Type': 'application/json' } });
       setCompletion(response.data.completion);
     } catch (error) {
       console.error(error);
     }
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setData({ ...data, [name]: value });
   };
 
   return (
@@ -22,12 +30,16 @@ function App() {
       <h1>めちゃくちゃ回答が早いお問い合わせフォーム</h1>
       <Form onSubmit={handleSubmit}>
         <Form.Group controlId="formPrompt">
-          <Form.Label>お問合せ内容:</Form.Label>
-          <Form.Control type="text" value={prompt} onChange={(e) => setPrompt(e.target.value)} />
+          <Form.Label>お問い合わせ内容:</Form.Label>
+          <Form.Control type="text" name="prompt" value={data.prompt} onChange={handleInputChange} />
         </Form.Group>
-        <Button variant="primary" type="submit">送信</Button>
+        <Form.Group controlId="formEmail">
+          <Form.Label>お客様の連絡先メールアドレス:</Form.Label>
+          <Form.Control type="email" name="email" value={data.email} onChange={handleInputChange} />
+        </Form.Group>
+        <Button variant="primary" type="submit">Complete</Button>
       </Form>
-      <p style={{ marginTop: '20px' }}>返信: {completion}</p>
+      <p style={{ marginTop: '20px' }}>Completion: {completion}</p>
     </div>
   );
 }
